5ec0c072c37c00210a184f489d6276a06f049bc2
Alfarisky <riskymuhammadalfa2@gmail.com>
2026-08-24 20:18:21 +0700

    Add word data

---
+++ app/src/main/java/com/example/unscramble/data/WordsData.kt
@@ -0,0 +1,27 @@
+package com.example.unscramble.data
+
+/**
+ * Daftar kata yang digunakan dalam permainan.
+ */
+val allWords = listOf(
+    "animal",
+    "auto",
+    "an",
+    "birthday",
+    "book",
+    "car",
+    "cat",
+    "dog",
+    "elephant",
+    "fish",
+    "flower",
+    "food",
+    "game",
+    "happy",
+    "house",
+    "java",
+    "kotlin",
+    "music",
+    "orange",
+    "phone"
+)
