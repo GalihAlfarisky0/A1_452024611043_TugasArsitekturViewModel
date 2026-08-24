a75fd7e53c014274f278c7c5c3f6aaba3517fa87
Alfarisky <riskymuhammadalfa2@gmail.com>
2026-08-24 20:18:17 +0700

    Add MainActivity

---
+++ app/src/main/java/com/example/unscramble/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.unscramble
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.unscramble.ui.GameScreen
+import com.example.unscramble.ui.GameViewModel
+import com.example.unscramble.ui.theme.UnscrambleTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            UnscrambleTheme {
+                val gameViewModel: GameViewModel = viewModel()
+                GameScreen(
+                    gameViewModel = gameViewModel
+                )
+            }
+        }
+    }
+}
